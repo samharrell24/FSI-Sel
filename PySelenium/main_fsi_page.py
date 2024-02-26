6991bc055386f78d174b75d570289bce606a2ef7
sam <44710166+samharrell24@users.noreply.github.com>
2024-02-26 16:34:12 -0500

    Update main_fsi_page.py

--- PySelenium/main_fsi_page.py
+++ PySelenium/main_fsi_page.py
@@ -2,44 +2,46 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 import time
+
+def go_through_models(driver):
+    models = driver.find_elements(By.TAG_NAME, "a")
+    c = 0
+    for i in models:
+        if c == 26:
+            break
+        try:
+            i.click()
+            time.sleep(1)
+        except:
+            models = driver.find_elements(By.TAG_NAME, "a")
+            models[c].click()
+            time.sleep(1)
+        driver.back()
+        c+=1
 
 driver = webdriver.Chrome()
 wait = WebDriverWait(driver, 5)
+actions = ActionChains(driver)
 
 # Home Page
 driver.get("https://flightsafety.com")
-# career_top_menu = driver.find_element(By.CLASS_NAME, "header__secondary-nav")
-# career_top_menu.click()
 
-career_top_menu = driver.find_element(By.CLASS_NAME, "left")
-# print(career_top_menu)
-# print(len(career_top_menu))
-p = career_top_menu.find_elements(By.TAG_NAME, "a")
+# Move to Business & Commercial
+x = driver.find_element(By.XPATH, "/html/body/header/div/nav/section/ul/li[1]/a")
+actions.move_to_element(x)
+actions.click(x)
+actions.perform()
 
-p = career_top_menu.find_element(By.XPATH, "/html/body/header/div/nav/section/ul/li[1]/ul/li/div/div/div")
-print(p)
-pp = p.find_elements(By.XPATH, "/html/body/header/div/nav/section/ul/li[1]/ul/li/div/div/div/div[1]/ul")
-print(pp)
-# TODO: [6492:17032:0223/154347.010:ERROR:ssl_client_socket_impl.cc(974)] handshake failed; returned -1, SSL error code 1, net_error -101
-# TODO: Fix ^^ from going to output in terminal by accepting cookies
+business_and_commercial = driver.find_element(By.CLASS_NAME, "move-right")
+p = business_and_commercial.find_elements(By.TAG_NAME, "a")
 
-# elem = driver.find_elements_by_xpath('//*[@id="mw-content-text"]/div/ul')
-#     for each_ul in elem:
-#         all_li = each_ul.find_elements_by_tag_name("li")
-#         for li in all_li:
-#             list_text.append(li.text)
+for i in p:
+    i.click()
+    driver.back()
+    p = business_and_commercial.find_elements(By.TAG_NAME, "a")
 
-for i in pp:
-    print(i)
-    # i.click()
-    # time.sleep(2)
-    # driver.back()
-    # time.sleep(2)
-    # p = career_top_menu.find_elements(By.TAG_NAME, "a")
 
-# career_top_menu.click()
 
 time.sleep(4)
-# continue_button = driver.find_element(By.NAME, "submit")
-# continue_button.click()
