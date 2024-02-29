9d152543ae4d40e54c2b327d9cd788010570c93c
sam <44710166+samharrell24@users.noreply.github.com>
2024-02-29 17:27:10 -0500

    Update main_fsi_page.py

--- PySelenium/main_fsi_page.py
+++ PySelenium/main_fsi_page.py
@@ -66,4 +66,18 @@ for i in range(0,len(a)):
     a[i].click()
     driver.back()
 
+# myFlightSafety
+myfsi = driver.find_element(By.XPATH, "/html/body/header/section/nav/ul/li[1]/a[2]")
+myfsi.click()
+
+# Enter in login information
+username = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="input28"]')))
+username.send_keys("")
+next = driver.find_element(By.XPATH, '//*[@id="form20"]/div[2]/input')
+next.click()
+username = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="input62"]')))
+username.send_keys("")
+verify = driver.find_element(By.XPATH, '//*[@id="form54"]/div[2]/input')
+verify.click()
+
 time.sleep(4)
